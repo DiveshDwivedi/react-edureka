3e83290718493f096977c03f04feddec89638887
Divesh Dwivedi <id956910@dl0128.local>
2024-06-11 06:15:50 +0000

    fix: other temprature type

--- typescript/index.ts
+++ typescript/index.ts
@@ -13,11 +13,14 @@ function convert_temprature(temperature:number, type:string) {
     switch (type) {
         case 'C':
             console.log('Fahrenheit to Celsius ' + (temperature - 32) * 5/9);
+            console.log('Fahrenheit to Kelvin ' +(temperature + 459.67) * 5/9);
             break;
         case 'F':
             console.log('Celsius to Fahrenheit ' + (temperature * 9/5) + 32);
+            console.log('Celsius to Kelvin ' + temperature + 273.15);
         case 'K':
-            console.log('Celsius to Kelvin ' + (temperature + 273.15));
+            console.log('Kelvin to Celsius ' + (temperature - 273.15));
+            console.log('Kelvin to Fahrenheit ' + ((temperature * 9/5) - 459.67));
         default:
             break;
     }
