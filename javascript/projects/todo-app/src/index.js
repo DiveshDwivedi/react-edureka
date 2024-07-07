9a2b94294b3addf767bfd7d9a1c91994c6194c24
Divesh Dwivedi <dwivedidivesh21@gmail.com>
2024-07-07 12:46:54 +0530

    using local storage to store and fetch todo data

--- javascript/projects/todo-app/src/index.js
+++ javascript/projects/todo-app/src/index.js
@@ -16,4 +16,25 @@ function add_task() {
 
     // clear input field after enter value
     input_box.value = '';
+
+    save_data_local_storage();
 }
+
+list_container.addEventListener('click', function(e){
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+        save_data_local_storage();
+    } else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+        save_data_local_storage();
+    } 
+}, false);
+
+function save_data_local_storage() {
+    localStorage.setItem('data', list_container.innerHTML);
+}
+
+function get_task_local_storage() {
+    list_container.innerHTML = localStorage.getItem('data');
+}
+get_task_local_storage();
